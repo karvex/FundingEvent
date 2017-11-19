37e0c0edcaffbd3d1557a9fec3dcf2550d70d9af
Bogdan Djukic <bdjukic@gmail.com>
2017-11-19 15:29:44 +0100

    Updating the list of meetups.

--- web/src/ListMeetups.js
+++ web/src/ListMeetups.js
@@ -10,6 +10,70 @@ class ListMeetups extends Component {
     super();
   }
 
+  state = {
+    meetups: [],
+  }
+
+  getMeetups() {
+    let _this = this;
+
+    var meetupListData = [
+      {
+        img: 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/5ab70a8813bed2fe065ca7534513866145155873_2880x1620.jpg?quality=95&w=480',
+        title: "Why do I make art? To build time capsules for my heritage?",
+        speaker: 'Anna Johnson',
+        creator: ''
+      },
+      {
+        img: 'http://image.pbs.org/video-assets/pbs/ted-talks/209634/images/mezzanine_243.jpg.resize.800x450.jpg',
+        title: "How to transform apocalypse fatigue into action on global warming?",
+        speaker: 'Maria Boss',
+        creator: ''
+      },
+      {
+        img: 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/25a5bc18d2472308c8ed2bb401b4a497f49a0265_1600x1200.jpg?quality=89&w=800',
+        title: "What I learned serving time for a crime I didn't commit?",
+        speaker: 'Sandra Maple',
+        creator: ''
+      },
+      {
+        img: 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/110884_800x600.jpg?w=1200',
+        title: "How judges can show respect?",
+        speaker: 'John Leek',
+        creator: ''
+      },
+    ];
+
+    _this.setState({
+      meetups: meetupListData
+    })
+
+    window.contract.getMeetupCount({ from: window.signedInUser }, function (error, result) {
+      var meetupCount = result.c[0]
+      for (var i = 0; i < meetupCount; i++) {
+        window.contract.getMeetup(i, { from: window.signedInUser }, function (error, result) {
+          if (result != null) {
+            meetupListData.push({ img: "https://www.newstatesman.com/sites/all/themes/creative-responsive-theme/images/new_statesman_events.jpg", title: result[1], speaker: "", creator: result[0] })
+            _this.setState({
+              meetups: meetupListData
+            })
+          }
+        });
+      }
+    });
+  }
+
+  componentWillMount() {
+    this.getMeetups()
+  }
+
+  handleFund(address) {
+    alert(address)
+    window.contract.donate(address, { from: window.signedInUser }, function (error, result) {
+      alert("hello")
+    });
+  }
+
   render() {
     const styles = {
       root: {
@@ -23,45 +87,32 @@ class ListMeetups extends Component {
       },
     };
 
-    var meetupListData = [
-      {
-        img: 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/5ab70a8813bed2fe065ca7534513866145155873_2880x1620.jpg?quality=95&w=480',
-        title: "Why do I make art? To build time capsules for my heritage?",
-        speaker: 'Anna Johnson'
-      },
-      {
-        img: 'http://image.pbs.org/video-assets/pbs/ted-talks/209634/images/mezzanine_243.jpg.resize.800x450.jpg',
-        title: "How to transform apocalypse fatigue into action on global warming?",
-        speaker: 'Maria Boss',
-      },
-      {
-        img: 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/25a5bc18d2472308c8ed2bb401b4a497f49a0265_1600x1200.jpg?quality=89&w=800',
-        title: "What I learned serving time for a crime I didn't commit?",
-        speaker: 'Sandra Maple',
-      },
-      {
-        img: 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/110884_800x600.jpg?w=1200',
-        title: "How judges can show respect?",
-        speaker: 'John Leek',
-      },
-    ];
+    let meetups = this.state.meetups;
+    const meetupItems = []
+    for (var i = 0; i < meetups.length; i++) {
+      let address = meetups[i].creator;
+      meetupItems.push(
+        <GridTile
+          key={meetups[i].img}
+          title={meetups[i].title}
+          subtitle={<span>by <b>{meetups[i].speaker}</b></span>}
+          actionIcon={<FlatButton 
+            label="Fund" 
+            onClick={(event) => this.handleFund(address)}
+            primary={true} />}>
+          <img src={meetups[i].img} />
+        </GridTile>
+      );
+    }
 
     return (
       <div className="ListMeetups">
-        <br/>
+        <br />
         <div style={styles.root}>
           <GridList
             cellHeight={180}
             style={styles.gridList}>
-            {meetupListData.map((meetup) => (
-              <GridTile
-                key={meetup.img}
-                title={meetup.title}
-                subtitle={<span>by <b>{meetup.speaker}</b></span>}
-                actionIcon={<FlatButton label="Send funds" primary={true} />}>
-                <img src={meetup.img} />
-              </GridTile>
-            ))}
+            {meetupItems}
           </GridList>
         </div>
       </div>
